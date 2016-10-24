f1b1a99f9b8d4850825228f6d8f794b0c839aba1
GerardCL <gerardcl@gmail.com>
2016-10-25 01:28:55 +0200

    angular integrated to go /token-auth

---
+++ master-server/public/app-services/authentication.service.js
@@ -0,0 +1,54 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('AuthenticationService', Service);
+
+    function Service($http, $localStorage) {
+        var service = {};
+
+        service.Login = Login;
+        service.Logout = Logout;
+
+        return service;
+
+        function guid() {
+          function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+              .toString(16)
+              .substring(1);
+          }
+          return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+        }
+
+        var uuid = guid();
+
+        function Login(username, password, callback) {
+            $http.post('/token-auth', { username: username, password: password, uuid: uuid })
+                .success(function (response) {
+                    // login successful if there's a token in the response
+                    if (response.token) {
+                        // store username and token in local storage to keep user logged in between page refreshes
+                        $localStorage.currentUser = { username: username, uuid: uuid, token: response.token };
+
+                        // add jwt token to auth header for all requests made by the $http service
+                        $http.defaults.headers.common.Authorization = 'Bearer ' + response.token;
+
+                        // execute callback with true to indicate successful login
+                        callback(true);
+                    } else {
+                        // execute callback with false to indicate failed login
+                        callback(false);
+                    }
+                });
+        }
+
+        function Logout() {
+            // remove user from local storage and clear http auth header
+            delete $localStorage.currentUser;
+            $http.defaults.headers.common.Authorization = '';
+        }
+    }
+})();
